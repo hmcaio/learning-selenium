0aba4a28f68a59a872baaceeab248704aed65ef0
hmcaio <hmcaio@hotmail.com>
2022-06-19 23:09:48 -0300

    Add argparse, logging and support for Edge

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-# learning-selenium #
+# learning-selenium
 
 Project for learning Selenium.
 
-### References
+## Versions
 
-* https://www.selenium.dev/documentation/webdriver/getting_started/
-* https://nander.cc/using-selenium-within-a-docker-container
-* https://medium.com/slalom-technology/web-automation-how-to-set-up-selenium-so-that-it-runs-on-your-computer-docker-containers-and-6c438becd912
-* https://github.com/SergeyPirogov/webdriver_manager
+|                   | version       |
+| ----------------- | ------------- |
+| Python            | 3.10.1        |
+| Selenium          | 4.2.0         |
+| Webdriver Manager | 3.7.0         |
+| Firefox           | 101.0         |
+| Firefox driver    | 0.31.0        |
+| Edge              | 102.0.1245    |
+| Edge driver       | 102.0.1245.44 |
+
+## References
+
+* [https://www.selenium.dev/documentation/webdriver/getting_started/]()
+* [https://nander.cc/using-selenium-within-a-docker-container]()
+* [https://medium.com/slalom-technology/web-automation-how-to-set-up-selenium-so-that-it-runs-on-your-computer-docker-containers-and-6c438becd912]()
+* [https://github.com/SergeyPirogov/webdriver_manager]()
--- app.py
+++ app.py
@@ -1,34 +1,83 @@
-from dotenv import load_dotenv
+import argparse
+import enum
+import logging
+import logging.config
 from pathlib import Path
+
+import yaml
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+# with open('logging.yml', 'r') as f:
+#     config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(config)
+
+logger = logging.getLogger()
 
 load_dotenv(dotenv_path=Path("env/.env"))
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-# driver.implicitly_wait(time_to_wait=5)
+class Browsers(enum.Enum):
+    FIREFOX = 0
+    EDGE = 1
 
-driver.get("https://google.com")
 
-title = driver.title
-assert title == "Google"
+def create_driver(browser):
+    match browser:
+        case Browsers.FIREFOX.name:
+            logger.info("Using Firefox browser")
+            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        case Browsers.EDGE.name:
+            logger.info("Using Edge browser")
+            driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+        case other:
+            # Should not get here since browser valid values are limited by argparse, but just in case
+            logger.error("Browser not supported.")
+            raise Exception("Browser not supported.")
 
-driver.implicitly_wait(0.5)
+    logger.info("Driver created successfully")
+    return driver
 
-search_box = driver.find_element(by=By.NAME, value="q")
-search_button = driver.find_element(by=By.NAME, value="btnK")
 
-search_box.send_keys("Selenium")
-search_box.submit()
-# search_button.click()
+def run(browser):
+    driver = create_driver(browser)
 
-search_box = driver.find_element(by=By.NAME, value="q")
-value = search_box.get_attribute("value")
-assert value == "Selenium"
+    # driver.implicitly_wait(time_to_wait=5)
 
-input("Press any key to continue")
+    driver.get("https://google.com")
 
-driver.quit()
+    title = driver.title
+    assert title == "Google"
+
+    driver.implicitly_wait(0.5)
+
+    search_box = driver.find_element(by=By.NAME, value="q")
+    search_button = driver.find_element(by=By.NAME, value="btnK")
+
+    search_box.send_keys("Selenium")
+    search_box.submit()
+    # search_button.click()
+
+    search_box = driver.find_element(by=By.NAME, value="q")
+    value = search_box.get_attribute("value")
+    assert value == "Selenium"
+
+    input("Press any key to continue")
+
+    driver.quit()
+
+
+if __name__ == "__main__":
+    arg_parse = argparse.ArgumentParser(description="Demo Selenium script")
+    
+    arg_parse.add_argument("--browser", choices=Browsers._member_names_, default=Browsers.FIREFOX.name, help="Browser to use")
+    
+    args = arg_parse.parse_args()
+
+    run(args.browser)
